9c1cf2d81adf444f67ff6fff476595c497d7a043
Falandy <Falandyjean@gmail.com>
2023-08-11 08:14:07 -0400

    update

--- table.js
+++ table.js
@@ -1,25 +1,34 @@
 const readline=require('readline-sync');
-const valeur=readline.question("Entrer la table a calculer :")
+const valeur=readline.question("Entrer la table a calculer :")*1
 let resultat;
  
-/*Table multiplication*/
+console.log("Table multiplication");
 for(i=0;i<=9;i++){
-    resultat=`${valeur} x ${i} = ${valeur*i}`;
+    resultat=`${i} x ${valeur} = ${i*valeur}
+    `;
     console.log(resultat);
 }
-/*Table addition*/
+
+console.log("Table addition");
+
 for(i=0;i<=9;i++){
-    resultat=`${valeur} + ${i} = ${valeur+i}`;
+    resultat=`${i} + ${valeur} = ${i+valeur}
+    `;
     console.log(resultat);
 }
-/*Table division*/
+
+console.log("Table division");
+
 for(i=0;i<=9;i++){
-    resultat=`${valeur} / ${i} = ${valeur/i}`;
+    resultat=`${valeur} / ${i} = ${valeur/i}
+    `;
     console.log(resultat);
 }
-/*Table soustraction*/
+console.log("Table soustraction");
+
 for(i=0;i<=9;i++){
-    resultat=`${valeur} - ${i} = ${valeur-i}`;
+    resultat=`${valeur} - ${i} = ${valeur-i}
+    `;
     console.log(resultat);
 }
 
