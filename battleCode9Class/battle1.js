60176ebc6c45b9409703599e10830f25e2c5fd8b
Falandy <Falandyjean@gmail.com>
2023-08-12 19:16:11 -0400

    .

--- battleCode9Class/battle1.js
+++ battleCode9Class/battle1.js
@@ -3,10 +3,10 @@ const  readlineSync=require('readline-sync')
 console.log("Pwogram lan pral transfome teks ou bay lan en majiskil")
 
 let fraz=readlineSync.question("Antre fraz ou an la : ")
-let mot=fraz.split(" ")
+let mots=fraz.split(" ")
 let nouvoTeks=" "
 
-for (mot of fraz) {
+for (mot of mots) {
     const premyeMo = mot.charAt(0).toUpperCase()
     const resMo = mot.slice(1).toLowerCase()
     nouvoTeks += premyeMo + resMo + " "
