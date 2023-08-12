fc69cd37e146054f87da7e08cb1addac81921f74
Falandy <Falandyjean@gmail.com>
2023-08-12 19:05:36 -0400

    add contains

---
+++ battleCode9Class/battle1.js
@@ -0,0 +1,9 @@
+const  readlineSync=require('readline-sync');
+
+console.log("Pwogram lan pral transfome teks ou bay lan en majiskil"):
+
+let fraz=readlineSync.question("Antre fraz ou an la : ")
+
+fraz=fraz.toUpperCase
+
+console.log(fraz)
