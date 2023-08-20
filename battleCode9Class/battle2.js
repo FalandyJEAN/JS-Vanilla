af40b94387a4cc053f4d3d70ef9699cad531d742
Falandy <Falandyjean@gmail.com>
2023-08-20 19:32:56 -0400

    update

--- battleCode9Class/battle2.js
+++ battleCode9Class/battle2.js
@@ -1,14 +1,23 @@
-const readlineSync=require("readline-sync")
-console.log("Pwogram sa ap resevwaa yon chen karakte epi lap ranplase tout karakte ki plase devan yon voyel sof espas pa X.");
+const readlineSync = require("readline-sync")
+
+console.log("Pwogram sa ap resevwaa yon chen karakte epi lap ranplase tout konsonn k'apre konsonn yo ak X.")
 let voyel = ["a", "e", "i", "o", "u", "y"]
 let chenn = readlineSync.question("Antre chenn karakte ou an :").toLowerCase()
 
 let nouvoChenn = ""
+let consonantCount = 0
+
 for (let i = 0; i < chenn.length; i++) {
-    if (voyel.includes(chenn[i]) || chenn[i] === " ") {
-        nouvoChenn += chenn[i];
+    if (voyel.includes(chenn[i])) {
+        nouvoChenn += chenn[i]
+        consonantCount = 0
+    } else if (chenn[i] === " ") {
+        nouvoChenn += " "
     } else {
-        nouvoChenn += "X"
+        if (consonantCount < 2) {
+            nouvoChenn += "X"
+            consonantCount++
+        }
     }
 }
 
