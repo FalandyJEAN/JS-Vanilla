d564e55835f3ff4e0b0369ed47addccd02269631
Falandy <Falandyjean@gmail.com>
2023-08-21 19:02:07 -0400

    correction bingo

--- bingo.js
+++ bingo.js
@@ -1,28 +1,27 @@
-const readline = require("readline-sync");
-
 let mX = 5, mY = 5;
 let tablo = [];
-let chans = 2;
-let boulYo = [];
 
 function generateTable() {
   tablo = [];
   for (let x = 0; x < mX; x++) {
-    let ranje = [];
+    let row = [];
     for (let y = 0; y < mY; y++) {
       let nonb;
       do {
         nonb = Math.floor(Math.random() * 100);
-      } while (ranje.includes(nonb));
-      ranje.push(nonb);
+      } while (row.includes(nonb));
+      row.push(nonb);
     }
-    tablo.push(ranje);
+    tablo.push(row);
+    console.log(row.join('\t\t'));
   }
 }
 
 generateTable();
 
+console.log("Nonb ki soti yo se: ")
 setTimeout(() => {
+  let boulYo = [];
   for (let i = 0; i < 5; i++) {
     let randX, randY, nonb;
     do {
@@ -34,48 +33,16 @@ setTimeout(() => {
     tablo[randX][randY] = `⭐`;
   }
 
-  console.log("Nonb ki soti yo se: ", boulYo);
-  jweJwèt();
-}, 2000);
-
-function aficheTablo() {
-  for (let x = 0; x < mX; x++) {
-    console.log(tablo[x].join('\t\t'));
+  for (let i = 0; i < boulYo.length; i++) {
+    console.log(boulYo[i]);
   }
-}
 
-function jweJwèt() {
-  console.log(`Ou gen ${chans} chans pou jwe.`);
-
-  let apJwe = true;
-  while (apJwe && chans > 0) {
-    aficheTablo();
-    console.log(`Nonb ki soti yo se: ${boulYo}`);
-    let nonbItilizatè = parseInt(readline.question("Antre yon nonb nan tablo a:"));
-    if (boulYo.includes(nonbItilizatè)) {
-      tablo.forEach((ranje, endisRanje) => {
-        ranje.forEach((selil, endisSelil) => {
-          if (selil === nonbItilizatè) {
-            tablo[endisRanje][endisSelil] = `[${selil}]`;
-          }
-        });
-      });
-
-      boulYo.splice(boulYo.indexOf(nonbItilizatè), 1);
-
-      if (boulYo.length === 0) {
-        console.log("Ou gen BINGO! Ou se yon chèf.");
-        apJwe = false;
-      }
-    } else {
-      console.log(`Nonb ${nonbItilizatè} pa nan tablo a.`);
-      chans--;
-      if (chans > 0) {
-        console.log(`Ou gen ${chans} chans reskonsab.`);
-      } else {
-        console.log("Chans ou fini. Ou pa ka jwe ankò.");
-        apJwe = false;
+  for (let i = 0; i < mX; i++) {
+    for (let j = 0; j < mY; j++) {
+      if (boulYo.includes(tablo[i][j])) {
+        tablo[i][j] = `[${tablo[i][j]}]`;
       }
     }
+    console.log(tablo[i].join('\t\t'));
   }
-}
+}, 2000);
