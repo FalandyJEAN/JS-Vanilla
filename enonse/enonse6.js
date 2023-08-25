d78cd0ca5781bbd16325a405d157ea559f56ca9e
Falandy <Falandyjean@gmail.com>
2023-08-25 10:05:00 -0400

    .

--- enonse/enonse6.js
+++ enonse/enonse6.js
@@ -1,46 +1,49 @@
 class BankAccount {
-    constructor(accountNumber, accountHolder, initialBalance = 0) {
-        this.accountNumber = accountNumber;
-        this.accountHolder = accountHolder;
-        this.balance = initialBalance;
-    }
+  constructor(accountNumber, accountHolder, initialBalance = 0) {
+    this.accountNumber = accountNumber;
+    this.accountHolder = accountHolder;
+    this.balance = initialBalance;
+  }
 
-    deposit(amount) {
-        if (amount > 0) {
-            this.balance += amount
-            console.log(`Ou depoze ${amount} HTG sou kont ou,ki gen kom nimewo : ${this.accountNumber}`)
-        } else {
-            console.log("Depo ou an pa valid,antre yon kantite kob ki siperye a zewo.")
-        }
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance += amount
+      console.log(`Ou depoze ${amount} HTG sou kont ou,ki gen kom nimewo : ${this.accountNumber}`)
+    } else {
+      console.log("Depo ou an pa valid,antre yon kantite kob ki siperye a zewo.")
     }
+  }
 
-    withdraw(amount) {
-        if (amount > 0 && amount <= this.balance) {
-            this.balance -= amount
-            console.log(`Ou fe retre ${amount} HTG sou kont ou,ki gen kom nimewo : ${this.accountNumber}`)
-        } else {
-            console.log("Retre sa a pa valid,eseye anko.")
-        }
+  withdraw(amount) {
+    if (amount > 0 && amount <= this.balance) {
+      this.balance -= amount
+      console.log(`Ou fe retre ${amount} HTG sou kont ou,ki gen kom nimewo : ${this.accountNumber}`)
+    } else {
+      console.log("Retre sa a pa valid,eseye anko.")
     }
+  }
 
-    getBalance() {
-        return this.balance
-    }
+  getBalance() {
+    return this.balance
+  }
 
-    toString() {
-        return `
+  toString() {
+    return `
         Account Number: ${this.accountNumber}
         Account Holder: ${this.accountHolder}
         Balance: ${this.balance.toFixed(2)} HTG`
-    }
+  }
 }
 
 const account1 = new BankAccount("00577883", "Falandy Jean", 700)
 
+
+let initialBalanceAccount = new BankAccount()
+console.log(`Kantite kob ou te genyen sou kont lan jis ka dat se ${initialBalanceAccount.balance} HTG
+`)
+
 account1.deposit(500)
 account1.withdraw(200)
-
-
 const balance = account1.getBalance()
 
 console.log(account1.toString())
