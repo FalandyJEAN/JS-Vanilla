a8cf6f39b0cc2758ce8d266807a14ea63a554d68
Falandy <Falandyjean@gmail.com>
2023-08-27 02:04:39 -0400

    first commit

--- battleCode9Class/battle3.js
+++ battleCode9Class/battle3.js
@@ -1,19 +1,6 @@
-function reverseAndShift(sequence, N) {
-    const arr = sequence.split(",").map(Number)
-    const L = arr.length
-    
-    for (let i = 0; i < L; i++) {
-        arr.reverse()
-        if (i !== L - 1) {
-            const lastElement = arr.pop()
-            arr.unshift(lastElement)
-        }
-    }
-    
-    const position = arr.indexOf(N)
-    console.log(`N = ${N}, pozisyon l = ${position}`)
+function reverse(sekans){
+    return sekans.split(",").reverse().join(",")
 }
 
-const sequence = "0,1,2,3,4"
-const N = 3
-reverseAndShift(sequence, N)
+let valeAn = reverse("1,2,3,4,5")
+console.log(valeAn)
