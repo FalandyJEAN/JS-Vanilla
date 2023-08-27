31d9fa98a890b3c21bfd95bee54272a9539604a5
Falandy <Falandyjean@gmail.com>
2023-08-27 02:44:56 -0400

    update

--- battleCode9Class/battle3.js
+++ battleCode9Class/battle3.js
@@ -1,6 +1,19 @@
-function reverse(sekans){
-    return sekans.split(",").reverse().join(",")
+function ranvese(chenn, N) {
+    const aranjman = chenn.split(",").map(Number)
+    const longe = aranjman.length
+    
+    for (let i = 0; i < longe; i++) {
+        aranjman.reverse()
+        if (i !== longe - 1) {
+            const denyeEleman = aranjman.pop()
+            aranjman.unshift(denyeEleman)
+        }
+    }
+    
+    const pozisyon = aranjman.indexOf(N);
+    console.log(`N = ${N}, pozisyon l = ${pozisyon}`);
 }
 
-let valeAn = reverse("1,2,3,4,5")
-console.log(valeAn)
+const chenn = "0,1,2,3,4";
+const N = 4;
+ranvese(chenn, N);
